321bbad2a79a71aae546effb6d4dbb72751b1186
Pragada Lakshmi Venkata Kalki Sainath <110445059+sainath1754@users.noreply.github.com>
2025-10-24 15:09:47 +0530

    Add files via upload

---
+++ java/DAY3.java
@@ -0,0 +1,11 @@
+package com.springbootlearning.com;
+import java.util.Scanner;
+public class DAY3 {
+	public static void main(String[] args) {
+	System.out.println("enter a number: ");
+	Scanner sc= new Scanner(System.in);
+	int p = sc.nextInt();
+	System.out.println("entered number is: "+p);
+	sc.close();
+	}
+}
