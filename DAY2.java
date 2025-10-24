0e631e5ef464f06dda561ce55028ab4cbcd62441
Pragada Lakshmi Venkata Kalki Sainath <110445059+sainath1754@users.noreply.github.com>
2025-10-24 14:19:21 +0530

    Add files via upload

---
+++ DAY2.java
@@ -0,0 +1,20 @@
+package com.springbootlearning.com;
+
+public class DAY2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Direct Conversion");
+		int a=8;
+		float b;
+		b=a;
+		System.out.println("typecasted value: "+b);
+		
+		System.out.println("InDirect Conversion");
+		float c = 8.0f;
+		int d;
+		d=(int)c;
+		System.out.println("typecasted value: "+d);
+		}
+
+}
